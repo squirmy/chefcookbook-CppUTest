8985fe0a6cf9963a5f922774b7368e7b18f832de
CapXilinx <melik-merkumians@acin.tuwien.ac.at>
2015-07-30 14:40:21 +0200

    Adds version constraints

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description 'Installs/Configures CppUTest'
 version '0.1.0'
 
 depends 'git', '~> 4.3'
-depends 'libtool'
-depends 'build-essential'
+depends 'libtool', '~> 0.1.0'
+depends 'build-essential', '~> 2.2.3'
